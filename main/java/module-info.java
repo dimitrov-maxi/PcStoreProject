de1a00a3aaa96164360749e4da15e660238f0d7e
dimitrov-maxi <92910102+dimitrov-maxi@users.noreply.github.com>
2022-03-28 16:59:02 -0700

    Delete main directory

--- main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.maxcomputers {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.maxcomputers to javafx.fxml;
-    exports com.example.maxcomputers;
-}
